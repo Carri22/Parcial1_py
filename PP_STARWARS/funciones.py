9b5aef9dcb48b6dd90c422dfcc15f4fc66004c66
Carri22 <108766795+Carri22@users.noreply.github.com>
2022-10-04 20:25:49 -0300

    Ejercicios de parcial

---
+++ PP_STARWARS/funciones.py
@@ -0,0 +1,103 @@
+import re
+import json
+
+def cargar_json(path:str)->list:
+    '''
+    carga un archivo de tipo json para utilizarlo
+    recive la ruta donde se encuentra el archivo
+    retorna una lista con los valores del archivo json
+    '''
+    with open(path, "r") as file:
+        listajson = json.load(file)
+    return listajson["results"]    
+
+def mostrar (lista:list):
+    '''
+    Imprime por consola cada indice una lista de forma en que se vea prolija
+    recive una lista
+    no retorna
+    '''
+    if len(lista)>0:
+        lista_mostrar=lista.copy()
+        for elemento in lista_mostrar:
+            mensaje = (f'Nombre: {elemento["name"]} | Height: {elemento["height"]} | Mass: {elemento["mass"]} | Gender: {elemento["gender"]} \n') 
+            print(mensaje)  
+
+def buscar_minimo(lista:list,key:str)->int:
+    '''
+    Busca el indice del valor mas bajo de una lista, dependiendo la key ingresada
+    recive una lista y un key 
+    retorna un int con el valor del indice del valor minimo
+    '''
+    if len(lista)>0:
+        lista_min=lista.copy()
+        i_min=0
+        i=0
+        for elemento in lista_min:
+            if int(elemento[key])<int(lista_min[i_min][key]):
+                i_min=i
+            i+=1
+        return i_min
+
+
+
+
+def buscar_maximo(lista:list,key:str)->int:
+    '''
+    Busca el indice del valor mas alto de una lista, dependiendo la key ingresada
+    recive una lista y un key 
+    retorna un int con el valor del indice del valor minimo
+    '''
+    if len(lista)>0:
+        lista_min=lista.copy()
+        i_max=0
+        i=0
+        for elemento in lista_min:
+            if int(elemento[key])>int(lista_min[i_max][key]):
+                i_max=i
+            i+=1
+        return i_max        
+
+def busca_personaje_mas_alto_por_genero(lista:list,genero:str):
+    '''
+    Busca el personaje mas alto por genero
+    recive una lista y un str con el genero "male" o "female"
+    retorna un mensaje diciendo quien es el personaje mas alto dependiendo el genero ingresado por parametro
+    '''
+    if len(lista)>0:
+        lista_altura_genero=lista.copy()
+        lista_genero=[]
+        for elemento in lista_altura_genero:
+            if elemento["gender"]==genero:
+                lista_genero.append(elemento)
+        indice=buscar_maximo(lista_genero, "height")
+        mensaje = (f'El personaje mas alto del genero {genero} es: {lista_genero[indice]["name"]} \n')
+        return mensaje
+        
+
+
+def listar_por_key(lista:list,key:str)->list:
+    '''
+    Ordena de menor a mayor una lista a partir del valor que represente la key ingresada
+    recive una lista a ordenar y una key
+    retorna una lista ordenada
+    '''
+    if len(lista)>0:
+        lista_a_ordenar = lista.copy()
+        lista_ordenada = []
+        while (len(lista_a_ordenar)):
+            index_minimo = buscar_minimo(lista_a_ordenar, key)
+            elemento = lista_a_ordenar.pop(index_minimo)
+            lista_ordenada.append(elemento)
+        return lista_ordenada 
+
+def exportar_csv(lista:list, path:str):
+    '''
+    Exporta un archivo csv a partir de una lista 
+    recive una lista 
+    no retorna
+    '''
+    with open(path, "w") as file:
+        for elemento in lista:
+            file.write(f'{elemento}\n')
+
---
+++ PP_STARWARS/main.py
@@ -0,0 +1,37 @@
+'''
+1 - Listar los personajes ordenados por altura
+2 - Mostrar el personaje mas alto de cada genero
+3 - 3 - Ordenar los personajes por peso
+4 - Armar un buscador de personajes 
+5 - Exportar lista personajes a CSV
+6 - Salir
+
+'''
+from funciones import *
+
+def starwars_app():
+    lista_personajes = cargar_json("PP_STARWARS/data.json")
+    while(True):
+        print("1 - Listar los personajes ordenados por altura\n2 - Mostrar el personaje mas alto de cada genero\n3 - Ordenar los personajes por peso\n4 - Armar un buscador de personajes\n5 - Exportar lista personajes a CSV\n6 - Salir\n")
+        respuesta = input()
+        if(respuesta=="1"):
+            print("1 - Listar los personajes ordenados por altura\n")
+            mostrar(listar_por_key(lista_personajes, "height"))
+        elif(respuesta=="2"):
+            print("2 - Mostrar el personaje mas alto de cada genero\n")
+            print (busca_personaje_mas_alto_por_genero(lista_personajes,"male"))
+            print (busca_personaje_mas_alto_por_genero(lista_personajes,"female"))
+        elif(respuesta=="3"):
+            print("3 - Ordenar los personajes por peso\n")
+            mostrar(listar_por_key(lista_personajes, "mass"))
+        elif(respuesta=="4"):
+            print("4 - Armar un buscador de personajes\n")
+        elif(respuesta=="5"):
+            print("5 - Exportar lista personajes a CSV\n")
+            exportar_csv(lista_personajes, "ArchivoCSV")
+        elif(respuesta=="6"):
+            break
+
+
+starwars_app()
+
